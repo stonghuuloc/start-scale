a4b80b82f5202da135b4eced76021e37e510507a
Do Khac Phong <phong@conceptual.studio>
2020-05-14 00:45:04 +0700

    alot of bug

--- static/front-end/build/webpack.config.dev.js
+++ static/front-end/build/webpack.config.dev.js
@@ -9,7 +9,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const utils = require('./utils')
 
 const HOST = 'localhost'
-const PORT = 8080
+const PORT = 8081
 
 module.exports = merge(baseConfig, {
   mode: 'development',
